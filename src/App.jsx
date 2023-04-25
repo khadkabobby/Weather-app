59cec7f8215eeb6506139299a181889b39053c71
bobby khadka <khadkaasmith2059@gmail.com>
2023-04-25 23:41:21 +0545

    Forecast also completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { WEATHER_API_URL } from "./api";
 import { WEATHER_API_KEY } from "./api";
 import Search from "./components/search/search";
 import { useState } from "react";
+import Forecast from "./components/Forecast/Forecast";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
@@ -32,6 +33,7 @@ function App() {
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
